245a7be2976ccc23646e790eb6e24b9ad33ae612
Todoropooh <65122420101@srru.ac.th>
2025-11-13 14:06:21 +0700

    Fix: Allow null origin for public HTML testing (CORS)

--- server.js
+++ server.js
@@ -38,6 +38,7 @@ const port = process.env.PORT || 3000;
 const corsOptions = {
     origin: [
         'http://localhost:5173', 
+        null,
         'http://localhost:3000',
         'http://localhost:3001', 
         'https://sut-park-a.vercel.app' 
